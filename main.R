cd8905b019453c57c12dcec77ba5c14342d192ed
Oskar Girardin <oskar.girardin@essec.edu>
2022-06-07 11:57:11 +0200

    added dataset

--- main.R
+++ main.R
@@ -1,1 +1,10 @@
+# IMport libraries
+library(readxl)
+
+
+
 # Import raw data
+setwd("C:/Users/Oskar/Documents/DSBA/Semester 3/Environmental Economics/Project/protected_areas")
+raw_data = read_excel("./WDPA.xlsx")
+
+data = raw_data
